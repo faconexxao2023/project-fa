178c484a6ea536d6b631a1995980e7d041af082b
faconexxao2023 <127159499+faconexxao2023@users.noreply.github.com>
2023-03-06 22:42:42 -0300

    Add files via upload

---
+++ Pages/Forms.js
@@ -0,0 +1,6 @@
+const injectForms = document.getElementById('redi-forms')
+injectForms.addEventListener('click' , handlePlugsForms)
+function handlePlugsForms() {
+  window.open('../Pages/PlugsInjetados.html')
+
+}
