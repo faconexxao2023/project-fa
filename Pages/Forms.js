d390c425954361c8504423ca33d69978f086bfcc
faconexxao2023 <127159499+faconexxao2023@users.noreply.github.com>
2023-03-09 01:36:20 -0300

    Delete Pages directory

--- Pages/Forms.js
+++
@@ -1,6 +0,0 @@
-const injectForms = document.getElementById('redi-forms')
-injectForms.addEventListener('click' , handlePlugsForms)
-function handlePlugsForms() {
-  window.open('../Pages/PlugsInjetados.html')
-
-}
